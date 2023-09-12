eae66abf2ac6bb99d9e9e97d27a99284d47b44ad
dsandy12 <dlsandy@asu.edu>
2023-09-12 08:35:50 -0700

    fixed missing curley brace

--- src/main/java/org/picmg/redfish_server_template/config/DefaultConfig.java
+++ src/main/java/org/picmg/redfish_server_template/config/DefaultConfig.java
@@ -28,5 +28,8 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class DefaultConfig {
     @PostConstruct
-    public void environmentMessage(){ // DEBUG: System.out.println("Opening the default profile"); }
+    public void environmentMessage(){ 
+        // DEBUG: System.out.println("Opening the default profile"); 
+    }
 }
+
