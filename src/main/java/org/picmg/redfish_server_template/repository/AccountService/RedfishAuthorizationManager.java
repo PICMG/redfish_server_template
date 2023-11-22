be5b7a4aa255e02928b4c90d9090b261a417e740
dsandy12 <dlsandy@asu.edu>
2023-11-22 06:41:29 -0700

    started work on action and actioninfo security and validation

--- src/main/java/org/picmg/redfish_server_template/repository/AccountService/RedfishAuthorizationManager.java
+++ src/main/java/org/picmg/redfish_server_template/repository/AccountService/RedfishAuthorizationManager.java
@@ -46,9 +46,18 @@ public final class RedfishAuthorizationManager implements AuthorizationManager<R
         if (uri.contains("IIoTNodes")) {
             return new AuthorizationDecision(true);
         }
-        PrivilegeTableEntry entry = privilegeTableService.getPrivilegeTableEntryFromUri(uri);
-        if ((entry != null) && (entry.isAuthorized(method, authorities)))
-            return new AuthorizationDecision(true);
+        if (uri.contains("/Actions")&&(method.equals("POST"))) {
+            // if the uri is for an action, and the request is for a post, use the privileges for the
+            // base object
+            PrivilegeTableEntry entry = privilegeTableService.getPrivilegeTableEntryFromUri(uri.substring(0,uri.indexOf("/Actions/")-1));
+            if ((entry != null) && (entry.isAuthorized(method, authorities)))
+                return new AuthorizationDecision(true);
+        } else {
+            // otherwise, use the privileges for the base object
+            PrivilegeTableEntry entry = privilegeTableService.getPrivilegeTableEntryFromUri(uri);
+            if ((entry != null) && (entry.isAuthorized(method, authorities)))
+                return new AuthorizationDecision(true);
+        }
 
         return new AuthorizationDecision(false);
     }
